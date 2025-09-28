d382bed76f75ff97e215946b77d0a3bb3634abe1
dfwpac <vipermilsims.buisness@gmail.com>
2025-09-28 14:35:25 +0300

    Create pyproject.py

---
+++ pyproject.py
@@ -0,0 +1,22 @@
+[project]
+name = "repl-nix-workspace"
+version = "0.1.0"
+description = "Add your description here"
+requires-python = ">=3.11"
+dependencies = [
+    "bcrypt>=5.0.0",
+    "cryptography>=46.0.1",
+    "eventlet>=0.40.3",
+    "flask>=3.1.2",
+    "flask-dance>=7.1.0",
+    "flask-limiter>=3.13",
+    "flask-login>=0.6.3",
+    "flask-socketio>=5.5.1",
+    "flask-sqlalchemy>=3.1.1",
+    "oauthlib>=3.3.1",
+    "psycopg2-binary>=2.9.10",
+    "pyjwt>=2.10.1",
+    "python-dotenv>=1.1.1",
+    "sqlalchemy>=2.0.43",
+    "werkzeug>=3.1.3",
+]
